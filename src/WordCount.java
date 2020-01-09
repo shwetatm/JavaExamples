7394f5c4c6f504e052f701458035a14f8bfb39f2
shwetatm <shwetatm@gmail.com>
2020-01-09 21:30:56 +0530

    Hi

---
+++ src/WordCount.java
@@ -0,0 +1,19 @@
+ /**
+ * 
+ */
+
+/**
+ * @author shweta
+ *
+ */
+public class WordCount {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
