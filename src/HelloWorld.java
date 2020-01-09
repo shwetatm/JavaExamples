2ad7abb779039adcc29c5d378e297bc100206658
shwetatm <shwetatm@gmail.com>
2020-01-09 21:11:52 +0530

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * @author shweta.tippanna
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hi");
+
+	}
+
+}
